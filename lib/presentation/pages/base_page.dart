999312ea5f3180875e40f3cf3867a110bde29d20
kkuczynski <krzysiek.kuczynski@icloud.com>
2021-12-09 12:15:26 +0100

    project structure, libs

---
+++ lib/presentation/pages/base_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+abstract class BasePage extends HookWidget {
+  const BasePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: buildContent(context),
+      ),
+    );
+  }
+
+  Widget buildContent(BuildContext context);
+}
